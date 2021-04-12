46bf4fac70dbaaa46f29a199c8d42bc57623c3cd
risenW <risenW@users.noreply.github.com>
2021-04-12 13:41:03 +0000

    deploy: a5df93ad093e53cc8431924df14aaff4e60dcb63

---
+++ _next/static/y5eCdqagSaEqjGRZS-mDi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6a0255e3110cb6ecb7c3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8935cce1a52e9d7b5e05.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
